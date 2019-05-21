365e303f6d856d4af3f51eb0e970d6e17b074b3c
Mateus Koppe <mateus.koppe@gmail.com>
2019-05-21 11:06:11 -0300

    Implement softdeletes

--- app/Note.php
+++ app/Note.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Note extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'title', 'description', 'schedule'
     ];
